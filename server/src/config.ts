5f974feefb079c17d3cd6a09590a014567ee31c9
Øivind Stensrud <oivind.stensrud@nav.no>
2022-01-17 13:44:41 +0100

    initial commit

---
+++ server/src/config.ts
@@ -0,0 +1,6 @@
+const APP_URL = process.env.APP_URL || 'localhost:3000'
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default {
+  BASE_PATH: '/aap-innsyn-dittnav',
+  APP_URL,
+};
---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Heading } from "@navikt/ds-react";
+
+export function App() {
+    return (<Heading size={"2xlarge"} level={"1"} >Hello world from AAP Innsyn!</Heading>);
+}
